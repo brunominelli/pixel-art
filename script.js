bda4905c936054baf17129ec031bfd9f9879636e
Bruno Minelli <minellibruno@hotmail.com>
2021-12-10 12:01:01 -0300

    Entrega do requisito 09.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const colorPalette = document.querySelectorAll('.color');
 const colorsClass = ['color-black', 'color-cyan', 'color-yellow', 'color-magenta'];
 const colorsNames = ['black', 'cyan', 'yellow', 'magenta'];
+const buttonClear = document.getElementById('clear-board');
 //console.log(colorPalette);
 let defaultColor = colorsNames[0];
 
@@ -8,15 +9,15 @@ for (let index = 0; index < colorPalette.length; index += 1) {
   colorPalette[index].style.backgroundColor = colorsClass[index];
 }
 
-function removeSelected() {
+function removeSelectedColor() {
   const selected = document.querySelector('.selected');
   selected.classList.remove('selected');
 }
 
-function addSelected() {
+function addSelectedColor() {
   for (let index = 0; index < colorPalette.length; index += 1) {
     colorPalette[index].addEventListener('click', () => {
-      removeSelected();
+      removeSelectedColor();
       defaultColor = colorsNames[index];
       colorPalette[index].className = `color ${colorsClass[index]} selected`;
       console.log(defaultColor);
@@ -25,16 +26,27 @@ function addSelected() {
    
 }
 
-document.addEventListener('click', function(event) {
-  if (event.target.classList.contains('pixel')) {
-    for (let index = 0; index <= colorsNames.length; index += 1) {
-      if (event.target.style.backgroundColor !== colorsNames[index]) {
-        event.target.style.backgroundColor = defaultColor;
-      } else event.target.style.backgroundColor = 'white';
+function fillBoard() {
+  document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('pixel')) {
+      for (let index = 0; index <= colorsNames.length; index += 1) {
+        if (event.target.style.backgroundColor !== colorsNames[index]) {
+          event.target.style.backgroundColor = defaultColor;
+        } else event.target.style.backgroundColor = 'white';
+      }
     }
-  }
-});
+  });
+}
 
+function clearBoard() {
+  buttonClear.addEventListener('click', function(event) {
+    const pixel = document.getElementsByClassName('pixel');
+      for (let index = 0; index < pixel.length; index += 1) {
+        pixel[index].style.backgroundColor = 'white';
+      }
+    });
+}
 
-addSelected();
-//fillPixel();
+addSelectedColor();
+fillBoard();
+clearBoard();
